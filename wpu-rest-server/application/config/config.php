6c52d67394c4fa6a491563684a303a8e4ee5909a
wildalestari <wildalestari2021@gmail.com>
2025-06-02 03:41:25 +0700

    update wpu-rest client

--- wpu-rest-server/application/config/config.php
+++ wpu-rest-server/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rest-api/wpu-rest-server/';
+$config['base_url'] = 'http://localhost/rest-api-fixs/wpu-rest-server/';
 
 /*
 |--------------------------------------------------------------------------
